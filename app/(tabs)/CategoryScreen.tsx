4664b8015553d232e2e608f0bec34aa71c91c327
kunal <natwarlaluzumaki@gmail.com>
2025-02-01 03:56:12 +0530

    add: category & notification  Screen

--- app/(tabs)/CategoryScreen.tsx
+++ app/(tabs)/CategoryScreen.tsx
@@ -1,12 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 import React from "react";
+import categoryData from "@/data/category.json";
+import { themes } from "@/themes";
+import Searchbar from "@/components/Searchbar";
 
 const CategoryScreen = () => {
   return (
-    <View>
-      <Text>CategoryScreen</Text>
+    <View style={styles.container}>
+      <Searchbar />
+      <ScrollView>
+        {categoryData.map((item, index) => {
+          return (
+            <View key={index} style={styles.categoryBoxs}>
+              <Text style={styles.text}>{item}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 10,
+    paddingBottom: 60,
+  },
+  categoryBoxs: {
+    width: "90%",
+    marginHorizontal: "auto",
+    height: 200,
+    borderRadius: 30,
+    backgroundColor: themes.secondary,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
 
 export default CategoryScreen;
--- app/(tabs)/NotificationScreen.tsx
+++ app/(tabs)/NotificationScreen.tsx
@@ -1,12 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import { width } from "@/utils/screen";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const NotficationScreen = () => {
   return (
-    <View>
-      <Text>NotficationScreen</Text>
+    <View style={style.container}>
+      {/* top bar */}
+      <View style={style.topBar}>
+        <Text style={style.text}>Notification</Text>
+        <TouchableOpacity>
+          <Ionicons name="notifications-off" size={30} color="black" />
+        </TouchableOpacity>
+      </View>
+
+      {/* nofications */}
+      <View style={{ margin: "auto" }}>
+        <Text style={style.text}>0 Notifications</Text>
+      </View>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topBar: {
+    width: width,
+    height: 80,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    flexDirection: "row",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+});
 
 export default NotficationScreen;
